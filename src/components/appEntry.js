f640546320749a60f0ff8a16cb052562c8e19e38
Alexei Humeniy <yo@ahumeniy.net>
2023-02-08 03:11:33 -0300

    add props

--- src/components/appEntry.js
+++ src/components/appEntry.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-function AppEntry() {
+function AppEntry(props) {
   return (
     <div>
       <Link to={`${props.app.Owner}/${props.app.Name}`}>{props.app.id}</Link>
